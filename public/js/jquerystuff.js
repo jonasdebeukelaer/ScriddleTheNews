1df9eed5f115f6e4763afa1a6b3dc2a8679faa52
Jonas De Beukelaer <jonasdebeukelaer@dyn1233-89.wlan.ic.ac.uk>
2014-10-16 21:27:25 +0100

    added loads of shit

---
+++ public/js/jquerystuff.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+	
+	console.log('Hello!')
+
+	$('.pic').click(function(){
+		$('.pic').fadeOut('slow');
+	});
+
+	$.ajax({
+		url:'http://graph.facebook.com/jeshuamaxey',
+		method:'GET'
+	}).success(function(data){
+		console.log(data)
+	})
+
+	$('.getInfo').click(function(){
+		var user = $('.fader').val()
+		console.log(user)
+		var username = 'http://graph.facebook.com/' + user
+		$.ajax({
+			url:username,
+			method:'GET'
+		}).success(function(data){
+			console.log(data)
+
+		})		
+
+	});
+
+})
+
