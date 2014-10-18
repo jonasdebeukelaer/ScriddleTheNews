7856a2eff431c27a9ae878fabd22ad9f3d38bd97
Scott Adams <s_j_adams@me.com>
2014-10-18 18:38:26 +0100

    Scotts Update:
    Added two post requests to index.js to post and return comments for a given paragraph

--- routes/index.js
+++ routes/index.js
@@ -6,11 +6,28 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
+
+var comments = [];						// creates comments container: array of comments hashes
+
+
+// When a paragraph is clicked to view comments
 router.post('/', function(req, res) {
-  var data = req.body
-  var product = (parseInt(data.num1) * parseInt(data.num2)).toString()
-  console.log(product)
-  res.send(product)
+	var pnum = req.body;						// gets pnum
+    res.send(comments[pnum]);					// returns comments for relevant pnum
+}
+
+
+
+// When a comment is posted
+router.post('/', function(req, res) {
+  var commentIn = req.body;						// imports comment in json format
+
+  if (!comments[commentIn.pnum]) {				// if no comments exist for that pnum
+  	comments[commentIn.pnum] = [commentIn];		// create array for that pnum containing the comment
+  } else {										// if a comment already exists for that pnum
+	comments[commentIn.pnum].push(commentIn);	// add incoming comment to array for that pnum
+  }
+  res.send(comments[pnum])						// returns comments for relevant pnum
 });
 
 module.exports = router;
